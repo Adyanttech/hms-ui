0982fc0f7d8806812f30cc63e636bdf299794ddc
c0k06tf <chekka.kumar@walmart.com>
2025-04-06 14:49:32 +0530

    - HMS UI Project setup. - Dashboard Page. - Login Page

---
+++ src/client/components/SideNav/SideNav.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { PanelMenu } from 'primereact/panelmenu';
+
+import SideNavStyles from "./SideNav.module.scss";
+
+const Sidenav = () => {
+  // const adminInfo = useSelector(state => state.application.adminInfo );
+  // const { displayName, adminGroups, mail } = adminInfo || {};
+  const router = useRouter();
+  const checkExpandedNav = ["providers", "products", "documents", "navigations"];
+
+  const items = [     
+    {
+      label: 'Dashboard',
+      icon: 'pi pi-list',
+      command: () => {
+        router.push('/dashboard');
+      },
+      expanded: router.pathname === "/dashboard"? true : false
+    },
+    {
+      label: 'Out Patients',
+      icon: 'pi pi-user',
+      command: () => {
+        router.push('/out_patients');
+      },
+      expanded: router.pathname === "/out_patients"? true : false
+    },
+    {
+      label: 'Prescriptions',
+      icon: 'pi pi-book',
+      command: () => {
+        router.push('/prescriptions');
+      },
+      expanded: router.pathname === "/prescriptions"? true : false
+    },
+    {
+      label: 'Appointments',
+      icon: 'pi pi-user-plus',
+      command: () => {
+        router.push('/appointments');
+      },
+      expanded: router.pathname === "/appointments"? true : false
+    }];
+
+  return (
+    <div className={SideNavStyles.sideNavContainer}>
+      <div>
+        <PanelMenu model={items} />
+      </div>
+    </div>
+  );
+}
+
+
+export default Sidenav;
---
+++ src/client/store/index.js
@@ -0,0 +1,34 @@
+// import applicationReducer from "./reduxSlices/applicationSlice";
+// import conditionalUsersReducer from "./reduxSlices/conditionalUsersSlice";
+import { configureStore } from "@reduxjs/toolkit";
+// import subscriptionReducer from "./reduxSlices/subscriptionSlice";
+// import blogReducer from "./reduxSlices/blogSlice";
+// import overviewReducer from "./reduxSlices/overviewSlice";
+// import dashboardFiltersReducer from "./reduxSlices/dashboardFiltersSlice";
+// import manageUsersReducer from "./reduxSlices/manageUsersSlice";
+// import detailsReducer from "./reduxSlices/detailsSlice";
+// import providersReducer from "./reduxSlices/providersSlice";
+// import productReducer from "./reduxSlices/productSlice";
+// import documentReducer from "./reduxSlices/documentSlice";
+// import navigationReducer from "./reduxSlices/navigationSlice"
+
+export const reducer = {
+    // application: applicationReducer,
+    // subscription: subscriptionReducer,
+    // conditionalUsers: conditionalUsersReducer,
+    // blog: blogReducer,
+    // overview: overviewReducer,
+    // dashboardFilters: dashboardFiltersReducer,
+    // manageUsers: manageUsersReducer,
+    // details: detailsReducer,
+    // providers: providersReducer,
+    // product: productReducer,
+    // navigation: navigationReducer,
+    // document: documentReducer
+}
+
+const rootReducer = configureStore({
+    reducer: reducer
+});
+
+export default rootReducer;
